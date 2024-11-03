b866ca6f7cdc19b59f86581f5f0c369b1ea99827
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-11-03 17:15:44 +0300

    Up

---
+++ 03_Day/examples/examples001.js
@@ -0,0 +1,34 @@
+let firstName = "Murat";
+let lastName = "Melek";
+let country = "Holland";
+let city = "Amsterdam";
+let age = 32;
+let isMarried = true;
+let job = "Software Developer"; // job değişkeni tanımlandı
+
+let sentence = `I am ${firstName} ${lastName}. I am from ${country}. I live in ${city}. I am ${age} years old. I am ${job}. I am married: ${isMarried}.`;
+console.log(sentence);
+
+// Output: I am Murat Melek. I am from Holland. I live in Amsterdam. I am 32 years old. I am Software Developer. I am married: true.
+
+console.log(typeof firstName); // string
+console.log(typeof lastName); // string
+console.log(typeof country); // string
+console.log(typeof city); // string
+console.log(typeof age); // number
+console.log(typeof isMarried); // boolean
+
+console.log("10"===10)
+
+console.log(parseInt("10")===10)
+
+console.log("9.81"===10)
+
+console.log(4 === 4);
+
+console.log(4 == 4);
+
+console.log(4 != 4);
+
+console.log(4 > 4);
+
