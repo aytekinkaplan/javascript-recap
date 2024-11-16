53b724b461478d9f9809c7a5fea4de30b5e9f16d
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-11-16 21:20:25 +0300

    Up

--- 08_Day/objects.js
+++ 08_Day/objects.js
@@ -10,3 +10,18 @@ function letsLearnScope() {
 console.log(a, b); // accessible
 
 letsLearnScope(), console.log(a, b);
+
+//scope.js
+let a = "JavaScript"; // is a global scope it will be found anywhere in this file
+let b = 10; // is a global scope it will be found anywhere in this file
+function letsLearnScope() {
+  console.log(a, b); // JavaScript 10, accessible
+  if (true) {
+    let a = "Python";
+    let b = 100;
+    console.log(a, b); // Python 100
+  }
+  console.log(a, b);
+}
+letsLearnScope();
+console.log(a, b); // JavaScript 10, accessible
