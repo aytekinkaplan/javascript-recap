6419ede747e6bbcbc92265443584b1c37f59c0aa
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-11-19 00:02:11 +0300

    Up

--- 09_Day/higher_order_functions.js
+++ 09_Day/higher_order_functions.js
@@ -1,6 +1,25 @@
 // Higher Order Function
 
 // Example 1: Returning a function from a function
-const callback(n)=>{
-  return n**2;
-}
+const callback = (n) => {
+  return n ** 2;
+};
+
+const cube = (callback, n) => {
+  return callback(n) * n;
+};
+
+console.log(cube(callback, 3));
+
+const numbers = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
+
+const sumArray = (arr) => {
+  let sum = 0;
+  const callBack = (element) => {
+    sum += element;
+  };
+  arr.forEach(callBack);
+  return sum;
+};
