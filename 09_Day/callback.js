fc7fc51c9c030163e0ef282af14690e35bc5e464
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-11-20 00:46:46 +0300

    Up

--- 09_Day/callback.js
+++ 09_Day/callback.js
@@ -4,3 +4,20 @@ function sayHello() {
 setInterval(sayHello, 1000);
 
 setTimeout(() => console.log("Hello"), 1000); // 1 set;
+
+// Functional Programming
+
+const sayHello = () => console.log("Hello");
+setInterval(sayHello, 1000);
+
+setTimeout(() => console.log("Hello"), 1000);
+
+const sayHello = () => console.log("Hello");
+setInterval(sayHello, 1000);
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const filteredArr = arr.filter((item) => item % 2 === 0);
+console.log(filteredArr);
+
+const arrayed = arr.forEach((item) => item % 2 === 0);
+console.log(arrayed);
