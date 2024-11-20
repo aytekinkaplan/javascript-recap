5b33b1348589e7bd41eb0a7416d944a71451e846
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-11-20 22:33:24 +0300

    Up

---
+++ 09_Day/map.js
@@ -0,0 +1,4 @@
+const countries = ["Finland", "Denmark", "Sweden", "Norway", "Iceland"];
+const nameOfCountries = countries.forEach((country) => console.log(country));
+
+console.log(nameOfCountries);
