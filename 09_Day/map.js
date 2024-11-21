273936231b6b8905b78a02c0efcb00c1b884b15f
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-11-21 07:04:03 +0300

    Up

--- 09_Day/map.js
+++ 09_Day/map.js
@@ -2,6 +2,9 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const doubledNumbers = numbers.map((num) => num * 2);
 console.log("Doubled numbers:", doubledNumbers);
 
+const squaredNumbers = numbers.map((num) => num ** 2);
+console.log("Squared numbers:", squaredNumbers);
+
 const names = ["Alice", "Bob", "Charlie", "David", "Eve"];
 const capitalizedNames = names.map((name) => name.toUpperCase());
 console.log("Capitalized names:", capitalizedNames);
