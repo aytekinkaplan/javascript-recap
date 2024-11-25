726e8d997588542f7c91ba81ff8a366cb07a399f
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-11-25 20:53:27 +0300

    Up

--- 10_Day/sets_and_maps.js
+++ 10_Day/sets_and_maps.js
@@ -8,27 +8,3 @@ const companies = new Set([
   "Amazon",
 ]);
 console.log(companies);
-console.log(companies.size);
-
-companies.add("Twitter");
-console.log(companies);
-
-companies.delete("Oracle");
-console.log(companies);
-
-console.log(companies.has("Facebook"));
-
-const itCompanies = [
-  "Facebook",
-  "Google",
-  "Microsoft",
-  "Apple",
-  "IBM",
-  "Oracle",
-  "Amazon",
-];
-const itCompaniesSet = new Set(itCompanies);
-console.log(itCompaniesSet);
-
-const itCompaniesArray = [...itCompaniesSet];
-console.log(itCompaniesArray);
