68b54fea592aaa7dee161d7c763bdc86702e4903
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-11-25 20:57:38 +0300

    Up

--- 10_Day/sets_and_maps.js
+++ 10_Day/sets_and_maps.js
@@ -8,3 +8,35 @@ const companies = new Set([
   "Amazon",
 ]);
 console.log(companies);
+
+const allLanguages = [
+  "English",
+  "Spanish",
+  "French",
+  "German",
+  "English",
+  "Spanish",
+  "French",
+  "German",
+  "Arabic",
+  "Portuguese",
+  "Italian",
+  "Chinese",
+  "Japanese",
+  "Korean",
+  "Arabic",
+  "Portuguese",
+  "Italian",
+  "Chinese",
+  "Japanese",
+  "Korean",
+  "Arabic",
+  "Portuguese",
+  "Italian",
+  "Chinese",
+  "Japanese",
+  "Korean",
+];
+
+const uniqueLanguages = new Set(allLanguages);
+console.log(uniqueLanguages);
