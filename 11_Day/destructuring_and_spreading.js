8fa8caf0c0898e4dd3dfabedfa715cd49f852a90
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-11-26 19:57:19 +0300

    Up

--- 11_Day/destructuring_and_spreading.js
+++ 11_Day/destructuring_and_spreading.js
@@ -4,3 +4,15 @@ const [first, second, ...rest] = numbers;
 console.log(first); // 1
 console.log(second); // 2
 console.log(rest); // [3, 4, 5, 6, 7, 8, 9, 10]
+
+// Destructing Objects
+const person = { name: "John", age: 30, city: "New York" };
+const { name, age, city } = person;
+console.log(name); // "John"
+console.log(age); // 30
+console.log(city); // "New York"
+
+// Spreading Objects
+const person1 = { name: "John", age: 30 };
+const person2 = { ...person1, city: "New York" };
+console.log(person2); // { name: "John", age: 30, city: "New York" }
