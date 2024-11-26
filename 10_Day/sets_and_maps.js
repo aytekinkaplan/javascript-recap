ebf84107579d5c128c5dbdaa8225cb8ebb1b7c87
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-11-26 19:06:45 +0300

    Up

--- 10_Day/sets_and_maps.js
+++ 10_Day/sets_and_maps.js
@@ -1,3 +1,4 @@
+// Creating a set of companies
 const companies = new Set([
   "Facebook",
   "Google",
@@ -7,8 +8,9 @@ const companies = new Set([
   "Oracle",
   "Amazon",
 ]);
-console.log(companies);
+console.log("Companies Set:", companies);
 
+// Creating a set of unique languages
 const allLanguages = [
   "English",
   "Spanish",
@@ -37,79 +39,65 @@ const allLanguages = [
   "Japanese",
   "Korean",
 ];
-
 const uniqueLanguages = new Set(allLanguages);
-console.log(uniqueLanguages);
-console.log(typeof uniqueLanguages);
+console.log("Unique Languages Set:", uniqueLanguages);
+console.log("Type of uniqueLanguages:", typeof uniqueLanguages);
 
+// Converting the set to an array
 const uniqueLanguagesArray = Array.from(uniqueLanguages);
-console.log(uniqueLanguagesArray);
-console.log(typeof uniqueLanguagesArray);
+console.log("Unique Languages Array:", uniqueLanguagesArray);
+console.log("Type of uniqueLanguagesArray:", typeof uniqueLanguagesArray);
 
 // Adding elements to a set
 companies.add("Twitter");
-console.log(companies);
+console.log("After adding Twitter:", companies);
 
 // Removing elements from a set
 companies.delete("Twitter");
-console.log(companies);
+console.log("After removing Twitter:", companies);
 
 // Checking if an element exists in a set
 const hasGoogle = companies.has("Google");
 console.log("Does Google exist in the set?", hasGoogle);
 
 // Iterating over a set
+console.log("Iterating over Companies Set:");
 for (const company of companies) {
   console.log(company);
 }
 
 // Converting a set to an array
 const companiesArray = Array.from(companies);
-console.log(companiesArray);
+console.log("Companies Array:", companiesArray);
 
 // Converting an array to a set
 const companiesSet = new Set(companiesArray);
-console.log(companiesSet);
+console.log("Recreated Companies Set:", companiesSet);
 
 // Finding the size of a set
 const size = companies.size;
-console.log("Size of the set:", size);
+console.log("Size of the Companies Set:", size);
 
 // Clearing a set
 companies.clear();
-console.log(companies);
+console.log("After clearing the Companies Set:", companies);
 
-// Creating a map
+// Creating a map of languages with unique values
 const languages = new Map([
   ["English", 1],
   ["Spanish", 2],
   ["French", 3],
   ["German", 4],
-  ["English", 5],
-  ["Spanish", 6],
-  ["French", 7],
-  ["German", 8],
-  ["Arabic", 9],
-  ["Portuguese", 10],
-  ["Italian", 11],
-  ["Chinese", 12],
-  ["Japanese", 13],
-  ["Korean", 14],
-  ["Arabic", 15],
-  ["Portuguese", 16],
-  ["Italian", 17],
-  ["Chinese", 18],
-  ["Japanese", 19],
-  ["Korean", 20],
-  ["Arabic", 21],
-  ["Portuguese", 22],
-  ["Italian", 23],
-  ["Chinese", 24],
-  ["Japanese", 25],
-  ["Korean", 26],
+  ["Arabic", 5],
+  ["Portuguese", 6],
+  ["Italian", 7],
+  ["Chinese", 8],
+  ["Japanese", 9],
+  ["Korean", 10],
 ]);
-console.log(languages);
+console.log("Languages Map:", languages);
 
+// Creating a map of countries and capitals
 const countries = [
   ["Finland", "Helsinki"],
   ["Sweden", "Stockholm"],
@@ -130,27 +118,52 @@ const countries = [
   ["Croatia", "Zagreb"],
   ["Cyprus", "Nicosia"],
   ["Czechia", "Prague"],
-  ["Denmark", "Copenhagen"],
-  ["Estonia", "Tallinn"],
   ["Faroe Islands", "Torshavn"],
-  ["Finland", "Helsinki"],
   ["France", "Paris"],
   ["Germany", "Berlin"],
   ["Greece", "Athens"],
   ["Ireland", "Dublin"],
   ["Italy", "Rome"],
-  ["Latvia", "Riga"],
-  ["Lithuania", "Vilnius"],
   ["Luxembourg", "Luxembourg"],
   ["Malta", "Valletta"],
   ["Netherlands", "Amsterdam"],
   ["Portugal", "Lisbon"],
   ["Romania", "Bucharest"],
-  ["Slovakia", "Bratislava"],
-  ["Slovenia", "Ljubljana"],
   ["Spain", "Madrid"],
-  ["Sweden", "Stockholm"],
 ];
-
 const countriesMap = new Map(countries);
-console.log(countriesMap);
+console.log("Countries Map:", countriesMap);
+
+// Accessing elements in a map
+const finland = countriesMap.get("Finland");
+console.log("Finland's Capital:", finland);
+
+// Adding elements to a map
+countriesMap.set("Austria", "Vienna");
+console.log("After adding Austria:", countriesMap);
+
+// Removing elements from a map
+countriesMap.delete("Sweden");
+console.log("After removing Sweden:", countriesMap);
+
+// Checking if an element exists in a map
+const hasPoland = countriesMap.has("Poland");
+console.log("Does Poland exist in the map?", hasPoland);
+
+// Iterating over a map
+console.log("Iterating over Countries Map:");
+for (const [country, capital] of countriesMap) {
+  console.log("Country:", country, "Capital:", capital);
+}
+
+// Converting a map to an array
+const countriesArray = Array.from(countriesMap);
+console.log("Countries Array from Map:", countriesArray);
+
+// Converting an array to a map
+const countriesMap2 = new Map(countriesArray);
+console.log("Recreated Countries Map from Array:", countriesMap2);
+
+// Finding the size of a map
+const mapSize = countriesMap.size;
+console.log("Size of the Countries Map:", mapSize);
