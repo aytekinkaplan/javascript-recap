66ce788d508caadd3cc09903a87625feb460da84
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-11-27 20:38:08 +0300

    Up

--- 11_Day/destructuring_and_spreading.js
+++ 11_Day/destructuring_and_spreading.js
@@ -91,3 +91,25 @@ const calculateAreaOfRectangle = (rectangle) => {
 };
 
 console.log(calculateAreaOfRectangle(rect));
+
+const todoList = [
+  {
+    task: "Prepare JS Test",
+    time: "4/1/2020 8:30",
+    completed: true,
+  },
+  {
+    task: "Give JS Test",
+    time: "4/1/2020 10:00",
+    completed: false,
+  },
+  {
+    task: "Assess Test Result",
+    time: "4/1/2020 1:00",
+    completed: false,
+  },
+];
+
+for (const { task, time, completed } of todoList) {
+  console.log(task, time, completed);
+}
