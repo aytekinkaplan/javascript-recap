6a5a46ee4668f885243a57a4f8e91f49175229b3
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-12-22 00:07:10 +0300

    Up

---
+++ 13_Console_object_methods/consloe_object_methods.js
@@ -0,0 +1,20 @@
+// Console Object Methods
+console.log("Hello world");
+console.error("This is an error");
+console.warn("This is a warning");
+console.info("This is an info");
+console.clear();
+console.table([1, 2, 3]);
+console.time("timer");
+console.timeEnd("timer");
+
+// Console Object Properties
+console.log(console);
+console.log(console.log);
+console.log(console.error);
+console.log(console.warn);
+console.log(console.info);
+console.log(console.clear);
+console.log(console.table);
+console.log(console.time);
+console.log(console.timeEnd);
