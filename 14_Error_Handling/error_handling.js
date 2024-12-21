b8eb0490bdf950c3f768c37983c05c2ff0f6feb2
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-12-22 00:07:22 +0300

    Up

---
+++ 14_Error_Handling/error_handling.js
@@ -0,0 +1,16 @@
+// Error Handling
+// try and catch
+
+try {
+  // Code that might throw an error
+} catch (error) {
+  // Code to handle the error
+}
+
+// throw new Error("This is an error");
+
+try {
+  throw new Error("This is an error");
+} catch (error) {
+  console.log(error);
+}
