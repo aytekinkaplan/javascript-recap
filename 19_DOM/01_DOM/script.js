58e0e9fc802c9ac24d0adf3b2e8a119cbf28d04c
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-12-26 01:06:21 +0300

    Up

--- 19_DOM/01_DOM/script.js
+++ 19_DOM/01_DOM/script.js
@@ -1,8 +1,44 @@
-// Getting Elements By Tag Name
-// Ensure the DOM is fully loaded before accessing elements
-document.addEventListener("DOMContentLoaded", () => {
-  const allTitles = document.getElementsByTagName("h1");
-  for (const title of allTitles) {
-    console.log(title);
-  }
+const lists = [
+  "Python",
+  "Java",
+  "C++",
+  "C#",
+  "JavaScript",
+  "Swift",
+  "Ruby",
+  "Go",
+  "Rust",
+  "PHP",
+  "Kotlin",
+  "TypeScript",
+  "Scala",
+  "Dart",
+  "Objective-C",
+  "C",
+  "R",
+  "Matlab",
+  "SwiftUI",
+  "Flutter",
+];
+
+const ul = document.getElementById("learning-list");
+
+lists.forEach((list) => {
+  // Create and style <li> element
+  const li = document.createElement("li");
+  li.textContent = "Learning " + list;
+  ul.appendChild(li);
+
+  // Click event for interaction
+  li.addEventListener("click", () => {
+    li.classList.add("completed");
+
+    setTimeout(() => {
+      li.classList.remove("completed");
+    }, 3000);
+
+    setTimeout(() => {
+      li.remove();
+    }, 5000);
+  });
 });
